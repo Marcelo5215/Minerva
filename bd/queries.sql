4848661e7aee4cc19ec704ec102894f2fd7ed1dd
Skalwalker <rekanobre@gmail.com>
2017-11-21 16:23:58 -0200

    Starting profile page

--- bd/queries.sql
+++ bd/queries.sql
@@ -1,9 +1,11 @@
-﻿INSERT INTO "Usuario" VALUES (1, 8000, 'rodriguinho@globo.com');
-INSERT INTO "Usuario" VALUES (2, 5500, 'fernandinho@gmail.com');
-INSERT INTO "Usuario" VALUES (3, 4751, 'aninha@hotmail.com');
+﻿INSERT INTO "Usuario" VALUES (1, 8000, 'rodriguinho@globo.com', 'Rodrigo');
+INSERT INTO "Usuario" VALUES (2, 5500, 'fernandinho@gmail.com', 'Fernando');
+INSERT INTO "Usuario" VALUES (3, 4751, 'aninha@hotmail.com', 'Aninha');
 
 INSERT INTO "Pergunta" VALUES (1, 'Como dobra um papel 12 vezes?', 'Blablabla',25,1);
 INSERT INTO "Pergunta" VALUES (2, 'Como se compila um programa em C?', 'Blablabla',30,1);
 INSERT INTO "Pergunta" VALUES (3, 'Quanto é 1+1?', 'Blablabla',23,2);
 INSERT INTO "Pergunta" VALUES (4, 'A terra é plana?', 'Blablabla',10,3);
 
+SELECT * FROM "Pergunta" AS p
+JOIN "Usuario" AS u ON 'p.usuarioID' = 'u.usuarioID';
